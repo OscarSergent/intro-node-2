5995492c1de5b7931b4584e932606071a072a217
Isnitho <user@Isnitho.com>
2024-10-22 14:37:23 +0200

    thinging around

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ app.use(cors())
 app.use(express.json())
 app.use(urlencoded({extended : false}))
 
-const movies = [
+const things = [
     {
         id : 1,
-        title : 'Dracula',
-        genre : 'Horror'
+        title : 'truc',
+        type : 'thing'    
     },
     {
         id : 2,
-        title : 'Old boy',
-        genre : 'Thriller',
+        title : 'machin',
+        type : 'thinger',
     },
     {
         id : 3,
-        title : 'Blade runner',
-        genre : 'Science-fiction',
+        title : 'bidule',
+        type : 'thingest',
     }
 ]
 
@@ -31,24 +31,24 @@ app.get('/', (req, res) => {
     return res.send(`Welcome to my API`)
 })
 
-app.get('/movies', (req, res) => {
-    return res.json(movies)
+app.get('/things', (req, res) => {
+    return res.json(things)
 })
 
-app.get('/movies/:movieID', (req, res) => {
-    let movieID = req.params.movieID
-    let movie = movies.find(el => el.id == movieID)
-    return res.json(movie)
+app.get('/things/:thingID', (req, res) => {
+    let thingID = req.params.thingID
+    let thing = things.find(el => el.id == thingID)
+    return res.json(thing)
 })
 
-app.post('/movies', (req, res) => {
-    let newMovie = {
-        id : movies.length+1,
+app.post('/things', (req, res) => {
+    let newThing = {
+        id : things.length+1,
         title : req.body.title,
-        genre : req.body.genre
+        type : req.body.type
     }
-    movies.push(newMovie)
-    return res.json(newMovie)
+    things.push(newThing)
+    return res.json(things)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT} :)`))
